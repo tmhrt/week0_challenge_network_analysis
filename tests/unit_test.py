bcb582addec4eb912aa51fa0bb9d034b26ebabff
ahunai <tmhrt.hussein.kedir.ali@gmail.com>
2023-12-01 18:36:10 +0300

    added tunit test

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,25 +1,15 @@
-import dataclasses
-import unittest
 import pandas as pd
-from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
 
-class DFTests(unittest.TestCase):
 
-    """ class for running unittests """
+def setUpModule():
+    global df 
+    df = pd.read_csv('movies.csv')
 
-    def setUp(self):
-        """ Your setUp """
-        TEST_INPUT_DIR = 'data/'
-        test_file_name =  'testdata.csv'
-        try:
-            data = pd.read_csv(INPUT_DIR + test_file_name,
-                sep = ',',
-                header = 0)
-        except IOError:
-            print('cannot open file')
-        self.fixture = dataclasses
 
-    def test_dataFrame_constructedAsExpected(self):
-        """ Test that the dataframe read in equals what you expect"""
-        foo = pd.DataFrame()
-        assert_frame_equal(self.fixture, foo)
+class TestData():
+    @dt.mandatory
+    def test_columns(self):
+        self.assertValid(
+            df.columns,
+            {'title', 'rating', 'year', 'runtime'},
+        )
